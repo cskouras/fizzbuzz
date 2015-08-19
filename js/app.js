524f2e7715e346cfccce015eeb8279926ab61db7
cskouras <callisteskouras@mac.com>
2015-08-19 11:36:16 -0400

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 $(document).ready(function() {
 
-for(var i=1; i<=100; i++) {
-  if(i%15===0) {
-      console.log("fizzbuzz")}
-  else if(i%3==0) {
-      console.log("fizz")}
-  else if(i%5==0) {
-     console.log("buzz")}
- else {console.log(i)}
+	$('.button').click(function(){
+		event.preventDefault()
+		var newNumber = $('.inputBox').val()
+		// changes string into integer
+		var num = +newNumber
+		$('.inputBox').val('');
+		$('.fizzbuzz').empty();
+	if ( num !== NaN && num >= 1 && num <= 100 && num % 1 == 0) {
+			// prevent number with decimal, number between 1 and 100, 
+		for(var i=1; i<=num; i++) {
+  			if(i%15===0) {
+     		 	console.log("fizzbuzz")
+  			$('.fizzbuzz').append('<li>FizzBuzz</li>')}
+  			else if(i%3==0) {
+      			console.log("fizz")
+  			$('.fizzbuzz').append('<li>Fizz</li>')}
+  			else if(i%5==0) {
+    			 console.log("buzz")
+ 			$('.fizzbuzz').append('<li>Buzz</li>')}
+ 			else {
+ 				console.log(i)
+ 			$('.fizzbuzz').append('<li>' + i + '</li>')}
 		}
-    
- }  )  
+
+ 	}
+ 	} ) 
+ 	}) 
+  
   
